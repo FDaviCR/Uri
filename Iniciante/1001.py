30649664dcc97d0b8f33f9209564c08e14c581d4
Davi Camilo Ribeiro <36652955+FDaviCR@users.noreply.github.com>
2020-09-02 02:01:37 -0300

    Inicio

---
+++ Iniciante/1001.py
@@ -0,0 +1,16 @@
+'''
+SOMA DE DOIS NÚMEROS
+  -----------------
+  Entrada: 10
+            9
+  Saída: X = 19
+  -----------------
+'''
+
+n1 = int(input())
+
+n2 = int(input())
+
+soma = (n1+n2)
+
+print("X =" ,soma)
